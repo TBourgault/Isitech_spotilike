87301b091790e4b07d18c6fc95db8cdfb46375be
Thibault Rochelemagne <thibault@rochelemagne.fr>
2018-04-19 15:26:47 +0200

    ArtisteDAO

---
+++ spotilike/SpotilikeClient/DAO/ArtisteDAO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpotilikeClient.Models;
+
+namespace SpotilikeClient.DAO
+{
+    class ArtisteDAO : IDAO<Artiste>
+    {
+        private string connectionString = @"Data Source=C:\Users\thiba\Desktop\Isitech_spotilike\db_spotilike.db; Version=3; FailIfMissing=True; Foreign Keys=True;";
+
+        public List<Artiste> findAll()
+        {
+            List<Artiste> artistes = new List<Artiste>();
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM Artiste";
+
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                    {
+                        using (SQLiteDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Artiste a = new Artiste();
+                                a.setId(int.Parse(reader["id"].ToString()));
+                                a.setFirstName(reader["first_name"].ToString());
+                                a.setLastName(reader["last_name"].ToString());
+                                a.setIsGroup(bool.Parse(reader["isGroup"].ToString()));
+                                a.setCountry(reader["country"].ToString());
+                                a.setCreatedAt(DateTime.Parse(reader["created_at"].ToString()));
+                                a.setGender(reader["gender"].ToString());
+
+                                artistes.Add(a);
+                            }
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            catch (SQLiteException e)
+            {
+                e.GetBaseException();
+            }
+
+            return artistes;
+        }
+    }
+}
